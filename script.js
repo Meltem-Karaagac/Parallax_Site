6e50ac1d2c0d4d0d1e2faa2da44cbb1e90bb8c34
ibrahimKanber <ikanber45@gmail.com>
2020-09-13 16:59:37 +0200

    sun and moon icons added

--- script.js
+++ script.js
@@ -7,4 +7,10 @@ toggle_btn.addEventListener("click",change)
 function change(){
     day.classList.toggle("day1");
     night.classList.toggle("night1");
+    var elements = document.querySelectorAll("section, section p");
+    var i;
+    for (i = 0; i < elements.length; i++) {
+        elements[i].classList.toggle("section-dark")
+    }
+
 }
